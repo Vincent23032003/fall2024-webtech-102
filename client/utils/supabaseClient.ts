a9d38cb4a45c28fe4f69f47d0bc51442d88f9939
Baré Vincent <vincent.bare@edu.ece.fr>
2024-11-17 17:23:46 +0100

    feat: part1 lab6

---
+++ client/utils/supabaseClient.ts
@@ -0,0 +1,15 @@
+// utils/supabaseClient.ts
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+if (!supabaseUrl) {
+  console.error('Missing NEXT_PUBLIC_SUPABASE_URL')
+}
+
+if (!supabaseAnonKey) {
+  console.error('Missing NEXT_PUBLIC_SUPABASE_ANON_KEY')
+}
+
+export const supabase = createClient(supabaseUrl!, supabaseAnonKey!)
